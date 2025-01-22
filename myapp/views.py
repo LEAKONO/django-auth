36f8f2e4d14e825e64ad011ae4bf19ff8a47c761
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2025-01-22 16:44:58 +0300

    updated views

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import viewsets, serializers, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Todo
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, UserSerializer
 
-# Todo viewset
+# User Registration Serializer
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)  # Create user with password
+        return user
+
+# User Registration View
+class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]  # Allow any user to register
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Todo Viewset (Authenticated)
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
