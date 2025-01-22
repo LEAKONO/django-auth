c7eec31ab02157548ca6ceeb43c566b24279e7f7
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2025-01-22 14:27:19 +0300

    made migrations

--- myapp/urls.py
+++ myapp/urls.py
@@ -10,5 +10,5 @@ router.register(r'todos', TodoViewSet, basename='todo')
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Token obtain
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Token refresh
-    path('', include(router.urls)),  # Include the todos routes
+    path('', include(router.urls)),  
 ]
--- myproject/settings.py
+++ myproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
